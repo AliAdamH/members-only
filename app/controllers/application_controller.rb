98e0ff7bec539b3e709216423b34b7de0013a179
Ali Hassaine <ali.hs@outlook.fr>
2022-03-22 23:10:23 +0000

    Update param sanitizing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_in, keys: [])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [])
-    devise_parameter_sanitizer.permit(:account_update, keys: [])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username name])
   end
 end
