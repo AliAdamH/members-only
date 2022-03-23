702d26b299e5940816c6840e020dea2aa34ac2b7
Ali Hassaine <ali.hs@outlook.fr>
2022-03-23 22:33:51 +0000

    Add post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, :body, presence: true
+  validates :title, length: { maximum: 30 }
+  validates :body, length: { maximum: 256 }
 end
