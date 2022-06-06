a753d452756412e3fa8aef1447ce8d5d82519ad7
Ali Hassaine <ali.hs@outlook.fr>
2022-06-06 17:08:19 +0000

    Clean up post creation flow

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,6 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # GET /posts/new
-  def new
-    @post = current_user.posts.build
-  end
 
   # GET /posts/1/edit
   def edit
@@ -22,11 +18,11 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_path, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        @posts = Post.all.order(created_at: :desc).includes(:user)
+        flash.now[:alert] = 'Oops your post contains some errors'
+        format.html { render :index, status: :unprocessable_entity }
       end
     end
   end
@@ -35,11 +31,10 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to posts_path, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
+        format.html { redirect_to posts_path, notice: 'Post was successfully updated.' }
       else
+        flash.now[:alert] = 'Oops your post contains some errors'
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -49,7 +44,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, except: %i[show]
+  resources :posts, except: %i[show new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   unauthenticated :user do
     root to: 'static#index'
